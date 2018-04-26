94adaddac8d1afe79ac6f6e0a2a45c77b1decb74
erikoichiki <aptrk.usam@gmail.com>
2018-04-26 14:08:43 +0900

    レッスン5の課題　修正

--- SlideshowApp/ResultViewController.swift
+++ SlideshowApp/ResultViewController.swift
@@ -9,34 +9,34 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
     @IBOutlet weak var gazou2: UIImageView!
     var x: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
         gazou2.image =   x  // Do any additional setup after loading the view.
-   
+        
         var transScale = CGAffineTransform()
         transScale = CGAffineTransform(scaleX: 2, y: 2)
         gazou2.transform = transScale
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,20 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-        @IBAction func unwind(_ segue: UIStoryboardSegue) {
-            if self.timer==nil{
-                self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(ViewController.onTimer), userInfo: nil, repeats: true)
-                zbutton.setTitle("停止", for: .normal)
-                susumu.isEnabled = false
-                modoru.isEnabled = false
-                
-            }else if self.timer != nil{
-                zbutton.setTitle("再生", for: .normal)
-                self.timer?.invalidate()
-                self.timer=nil
-                susumu.isEnabled = true
-                modoru.isEnabled = true
-                }
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        if self.timer==nil{
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(ViewController.onTimer), userInfo: nil, repeats: true)
+            zbutton.setTitle("停止", for: .normal)
+            susumu.isEnabled = false
+            modoru.isEnabled = false
+            
+        }else if self.timer != nil{
+            zbutton.setTitle("再生", for: .normal)
+            self.timer?.invalidate()
+            self.timer=nil
+            susumu.isEnabled = true
+            modoru.isEnabled = true
+        }
     }
     @IBAction func tapimage(_ sender: Any) {
         performSegue(withIdentifier: "kakudai",sender: nil)
@@ -39,7 +39,7 @@ class ViewController: UIViewController {
     @IBAction func go(_ sender: Any) {
         dispImageNo += 1
         displayImage()
-
+        
     }
     
     @IBAction func `return`(_ sender: Any) {
@@ -108,12 +108,12 @@ class ViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         let resultViewController:ResultViewController = segue.destination as! ResultViewController
-      
+        
         resultViewController.x = UIImage(named: imageNameArray[dispImageNo])
     }
-        
+    
     override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
